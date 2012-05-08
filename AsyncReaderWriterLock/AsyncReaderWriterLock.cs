0045ef3bf651204c94104e35a95e6a66e21950c5
Andrew Arnott <andrewarnott@gmail.com>
2012-05-08 16:34:39 -0700

    Initial checkin with several passing unit tests.

---
+++ AsyncReaderWriterLock/AsyncReaderWriterLock.cs
@@ -0,0 +1,236 @@
+﻿namespace AsyncReaderWriterLock {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Runtime.CompilerServices;
+	using System.Runtime.Remoting.Messaging;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <remarks>
+	/// TODO: 
+	///  * internally: sticky write locks, lock leasing, lock sharing
+	///    * consider: hooks for executing code at the conclusion of a write lock.
+	///  * externally: SkipInitialEvaluation, SuppressReevaluation
+	/// </remarks>
+	public class AsyncReaderWriterLock {
+		private readonly object syncObject = new object();
+
+		private readonly string logicalDataKey = Guid.NewGuid().ToString();
+
+		private readonly HashSet<Guid> lockHolders = new HashSet<Guid>();
+
+		private int lockState;
+
+		public AsyncReaderWriterLock() {
+		}
+
+		internal enum LockKind {
+			Read,
+			UpgradeableRead,
+			Write,
+		}
+
+		////internal LockTaskScheduler Read { get; private set; }
+
+		////internal LockTaskScheduler UpgradeableRead { get; private set; }
+
+		////internal LockTaskScheduler Writer { get; private set; }
+
+		public bool IsReadLockHeld {
+			get { return this.IsLockHeld(LockKind.Read); }
+		}
+
+		public bool IsUpgradeableReadLockHeld {
+			get { return this.IsLockHeld(LockKind.UpgradeableRead); }
+		}
+
+		public bool IsWriteLockHeld {
+			get { return this.IsLockHeld(LockKind.Write); }
+		}
+
+		public LockAwaitable ReadLockAsync(CancellationToken cancellationToken = default(CancellationToken)) {
+			return new LockAwaitable(this, LockKind.Read);
+		}
+
+		public LockAwaitable UpgradeableReadLockAsync(CancellationToken cancellationToken = default(CancellationToken)) {
+			return new LockAwaitable(this, LockKind.UpgradeableRead);
+		}
+
+		public LockAwaitable WriteLockAsync(CancellationToken cancellationToken = default(CancellationToken)) {
+			return new LockAwaitable(this, LockKind.Write);
+		}
+
+		private bool IsLockHeld(LockKind kind) {
+			object data = System.Runtime.Remoting.Messaging.CallContext.LogicalGetData(this.logicalDataKey);
+			if (this.lockState != 0 && data != null) {
+				lock (this.syncObject) {
+					return this.lockHolders.Contains((Guid)data);
+				}
+			}
+
+			return false;
+		}
+
+		private bool TryIssueLock(LockKind kind) {
+			bool issued = false;
+			lock (this.syncObject) {
+				switch (kind) {
+					case LockKind.Read:
+						if (this.lockState >= 0) {
+							this.lockState++;
+							issued = true;
+						}
+
+						break;
+					case LockKind.UpgradeableRead:
+						if (this.lockState == 0) {
+							this.lockState |= 0x40000000;
+							issued = true;
+						}
+
+						break;
+					case LockKind.Write:
+						if (this.lockState == 0) {
+							this.lockState = -1;
+							issued = true;
+						}
+
+						break;
+					default:
+						throw new Exception();
+				}
+			}
+
+			if (issued) {
+				var lockId = Guid.NewGuid();
+				CallContext.LogicalSetData(this.logicalDataKey, lockId);
+				this.lockHolders.Add(lockId);
+			}
+
+			return issued;
+		}
+
+		private void Release(LockKind kind) {
+			Guid lockId = (Guid)CallContext.LogicalGetData(this.logicalDataKey);
+			CallContext.LogicalSetData(this.logicalDataKey, null);
+
+			lock (this.syncObject) {
+				this.lockHolders.Remove(lockId);
+				switch (kind) {
+					case LockKind.Read:
+						this.lockState--;
+						break;
+					case LockKind.UpgradeableRead:
+						this.lockState &= ~0x4000000;
+						break;
+					case LockKind.Write:
+						this.lockState = 0;
+						break;
+					default:
+						throw new Exception();
+				}
+			}
+		}
+
+		public struct LockAwaitable {
+			private readonly AsyncReaderWriterLock lck;
+			private readonly LockKind kind;
+
+			internal LockAwaitable(AsyncReaderWriterLock lck, LockKind kind) {
+				this.lck = lck;
+				this.kind = kind;
+			}
+
+			public LockAwaiter GetAwaiter() {
+				return new LockAwaiter(this.lck, this.kind);
+			}
+		}
+
+		public struct LockAwaiter : INotifyCompletion {
+			private readonly AsyncReaderWriterLock lck;
+			private readonly LockKind kind;
+			private bool lockIssued;
+
+			internal LockAwaiter(AsyncReaderWriterLock lck, LockKind kind) {
+				this.lck = lck;
+				this.kind = kind;
+				this.lockIssued = false;
+			}
+
+			public bool IsCompleted {
+				get {
+					if (this.lockIssued) {
+						throw new Exception();
+					}
+
+					return this.lockIssued = this.lck.TryIssueLock(this.kind);
+				}
+			}
+
+			public void OnCompleted(Action continuation) {
+				if (this.lockIssued) {
+					throw new InvalidOperationException();
+				}
+
+				throw new NotImplementedException();
+			}
+
+			public LockReleaser GetResult() {
+				if (!this.lockIssued) {
+					throw new Exception();
+				}
+
+				return new LockReleaser(this.lck, this.kind);
+			}
+		}
+
+		public struct LockReleaser : IDisposable {
+			private readonly AsyncReaderWriterLock lck;
+
+			private readonly LockKind kind;
+
+			internal LockReleaser(AsyncReaderWriterLock lck, LockKind kind) {
+				this.lck = lck;
+				this.kind = kind;
+			}
+
+			public void Dispose() {
+				this.lck.Release(this.kind);
+			}
+		}
+
+		////internal class LockTaskScheduler : TaskScheduler {
+		////	protected override IEnumerable<Task> GetScheduledTasks() {
+		////		throw new NotImplementedException();
+		////	}
+
+		////	protected override void QueueTask(Task task) {
+		////		throw new NotImplementedException();
+		////	}
+
+		////	protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) {
+		////		throw new NotImplementedException();
+		////	}
+
+		////	public Awaiter GetAwaiter() {
+		////		return new Awaiter();
+		////	}
+
+		////	public struct Awaiter : INotifyCompletion {
+		////		public bool IsCompleted { get; private set; }
+
+		////		public void OnCompleted(Action continuation) {
+		////			throw new NotImplementedException();
+		////		}
+
+		////		public void GetResult() {
+		////		}
+		////	}
+		////}
+	}
+}
---
+++ AsyncReaderWriterLockTests/AsyncReaderWriterLockTests.cs
@@ -0,0 +1,141 @@
+﻿namespace AsyncReaderWriterLockTests {
+	using System;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using AsyncReaderWriterLock;
+	using System.Threading.Tasks;
+	using System.Threading;
+
+	[TestClass]
+	public class AsyncReaderWriterLockTests {
+		private AsyncReaderWriterLock asyncLock;
+
+		[TestInitialize]
+		public void Initialize() {
+			this.asyncLock = new AsyncReaderWriterLock();
+		}
+
+		[TestMethod]
+		public void NoLocksHeld() {
+			Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+			Assert.IsFalse(this.asyncLock.IsUpgradeableReadLockHeld);
+			Assert.IsFalse(this.asyncLock.IsWriteLockHeld);
+		}
+
+		#region Read tests
+
+		[TestMethod]
+		public async Task SimpleReadLock() {
+			Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+			using (await this.asyncLock.ReadLockAsync()) {
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+				await Task.Yield();
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+			}
+
+			Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+		}
+
+		[TestMethod]
+		public async Task ReadLockNotIssuedToAllThreads() {
+			var evt = new ManualResetEventSlim(false);
+			var otherThread = Task.Run(delegate {
+				evt.Wait();
+				Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+			});
+
+			using (await this.asyncLock.ReadLockAsync()) {
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+				evt.Set();
+				await otherThread;
+			}
+		}
+
+		[TestMethod]
+		public async Task ReadLockImplicitSharing() {
+			using (await this.asyncLock.ReadLockAsync()) {
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+
+				await Task.Run(delegate {
+					Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+				});
+
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+			}
+		}
+
+		[TestMethod]
+		public async Task ReadLockImplicitSharingCutOffByParent() {
+			Task subTask;
+			var outerLockReleased = new TaskCompletionSource<object>();
+			using (await this.asyncLock.ReadLockAsync()) {
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+
+				var subTaskObservedLock = new TaskCompletionSource<object>();
+				subTask = Task.Run(async delegate {
+					Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+					subTaskObservedLock.SetResult(null);
+					await outerLockReleased.Task;
+					Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+				});
+
+				await subTaskObservedLock.Task;
+			}
+
+			Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+			outerLockReleased.SetResult(null);
+			await subTask;
+		}
+
+		#endregion
+
+		#region UpgradeableRead tests
+
+		[TestMethod]
+		public async Task UpgradeableReadLockNoUpgrade() {
+			Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+			Assert.IsFalse(this.asyncLock.IsUpgradeableReadLockHeld);
+			using (await this.asyncLock.UpgradeableReadLockAsync()) {
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+				Assert.IsTrue(this.asyncLock.IsUpgradeableReadLockHeld);
+				await Task.Yield();
+				Assert.IsTrue(this.asyncLock.IsReadLockHeld);
+				Assert.IsTrue(this.asyncLock.IsUpgradeableReadLockHeld);
+			}
+
+			Assert.IsFalse(this.asyncLock.IsReadLockHeld);
+			Assert.IsFalse(this.asyncLock.IsUpgradeableReadLockHeld);
+		}
+
+		[TestMethod, Timeout(1000), Ignore]
+		public async Task UpgradeReadLock() {
+			using (await this.asyncLock.UpgradeableReadLockAsync()) {
+				Assert.IsFalse(this.asyncLock.IsWriteLockHeld);
+				using (await this.asyncLock.WriteLockAsync()) {
+					await Task.Yield();
+					Assert.IsTrue(this.asyncLock.IsWriteLockHeld);
+					Assert.IsTrue(this.asyncLock.IsUpgradeableReadLockHeld);
+				}
+
+				Assert.IsFalse(this.asyncLock.IsWriteLockHeld);
+			}
+		}
+
+		#endregion
+
+		#region Write tests
+
+		[TestMethod]
+		public async Task SimpleWriteLock() {
+			Assert.IsFalse(this.asyncLock.IsWriteLockHeld);
+			using (await this.asyncLock.WriteLockAsync()) {
+				Assert.IsTrue(this.asyncLock.IsWriteLockHeld);
+				await Task.Yield();
+				Assert.IsTrue(this.asyncLock.IsWriteLockHeld);
+			}
+
+			Assert.IsFalse(this.asyncLock.IsWriteLockHeld);
+		}
+
+		#endregion
+	}
+}
