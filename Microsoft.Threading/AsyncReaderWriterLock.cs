430f03f318f9cb7ba721db08440e36a9388ec594
Andrew Arnott <andrewarnott@gmail.com>
2012-05-22 08:05:28 -0700

    Added code comments.

--- Microsoft.Threading/AsyncReaderWriterLock.cs
+++ Microsoft.Threading/AsyncReaderWriterLock.cs
@@ -21,24 +21,80 @@
 	/// which requires that we get to execute code at the start of the continuation (whether we yield or not).
 	/// </remarks>
 	public class AsyncReaderWriterLock {
+		/// <summary>
+		/// The object to acquire a Monitor-style lock on for all field access on this instance.
+		/// </summary>
 		private readonly object syncObject = new object();
 
+		/// <summary>
+		/// A randomly generated GUID associated with this particular lock instance that will
+		/// be used as a key in the CallContext dictionary for storing the lock state for that
+		/// particular context.
+		/// </summary>
 		private readonly string logicalDataKey = Guid.NewGuid().ToString();
 
+		/// <summary>
+		/// The set of read locks that are issued and active.
+		/// </summary>
+		/// <remarks>
+		/// Many readers are allowed concurrently.  Also, readers may re-enter read locks (recursively)
+		/// each of which gets an element in this set.
+		/// </remarks>
 		private readonly HashSet<Awaiter> readLocksIssued = new HashSet<Awaiter>();
 
+		/// <summary>
+		/// The set of upgradeable read locks that are issued and active.
+		/// </summary>
+		/// <remarks>
+		/// Although only one upgradeable read lock can be held at a time, this set may have more
+		/// than one element because that one lock holder may enter the lock it already possesses 
+		/// multiple times.
+		/// </remarks>
 		private readonly HashSet<Awaiter> upgradeableReadLocksIssued = new HashSet<Awaiter>();
 
+		/// <summary>
+		/// The set of write locks that are issued and active.
+		/// </summary>
+		/// <remarks>
+		/// Although only one write lock can be held at a time, this set may have more
+		/// than one element because that one lock holder may enter the lock it already possesses 
+		/// multiple times.
+		/// Although this lock is mutually exclusive, there *may* be elements in the
+		/// <see cref="upgradeableReadLocksIssued"/> set if the write lock was upgraded from a reader.
+		/// Also note that some elements in this may themselves be upgradeable readers if they have
+		/// the <see cref="LockFlags.StickyWrite"/> flag.
+		/// </remarks>
 		private readonly HashSet<Awaiter> writeLocksIssued = new HashSet<Awaiter>();
 
+		/// <summary>
+		/// A queue of readers waiting to obtain the concurrent read lock.
+		/// </summary>
 		private readonly Queue<Awaiter> waitingReaders = new Queue<Awaiter>();
 
+		/// <summary>
+		/// A queue of upgradeable readers waiting to obtain a lock.
+		/// </summary>
 		private readonly Queue<Awaiter> waitingUpgradeableReaders = new Queue<Awaiter>();
 
+		/// <summary>
+		/// A queue of writers waiting to obtain an exclusive lock.
+		/// </summary>
 		private readonly Queue<Awaiter> waitingWriters = new Queue<Awaiter>();
 
+		/// <summary>
+		/// The source of the <see cref="Completion"/> task, which transitions to completed after
+		/// the <see cref="Complete"/> method is called and all issued locks have been released.
+		/// </summary>
 		private readonly TaskCompletionSource<object> completionSource = new TaskCompletionSource<object>();
 
+		/// <summary>
+		/// The queue of callbacks to invoke when the currently held write lock is totally released.
+		/// </summary>
+		/// <remarks>
+		/// If the write lock is released to an upgradeable read lock, these callbacks are fired synchronously
+		/// with respect to the writer who is releasing the lock.  Otherwise, the callbacks are invoked
+		/// asynchronously with respect to the releasing thread.
+		/// </remarks>
 		private readonly Queue<Func<Task>> beforeWriteReleasedCallbacks = new Queue<Func<Task>>();
 
 		/// <summary>
@@ -47,81 +103,204 @@
 		/// </summary>
 		private bool completeInvoked;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AsyncReaderWriterLock"/> class.
+		/// </summary>
 		public AsyncReaderWriterLock() {
 		}
 
+		/// <summary>
+		/// Flags that modify default lock behavior.
+		/// </summary>
 		[Flags]
 		public enum LockFlags {
+			/// <summary>
+			/// The default behavior applies.
+			/// </summary>
 			None = 0x0,
+
+			/// <summary>
+			/// Causes an upgradeable reader to remain in an upgraded-write state once upgraded,
+			/// even after the nested write lock has been released.
+			/// </summary>
+			/// <remarks>
+			/// This is useful when you have a batch of possible write operations to apply, which
+			/// may or may not actually apply in the end, but if any of them change anything,
+			/// all of their changes should be seen atomically (within a single write lock).
+			/// This approach is preferable to simply acquiring a write lock around the batch of
+			/// potential changes because it doesn't defeat concurrent readers until it knows there
+			/// is a change to actually make.
+			/// </remarks>
 			StickyWrite = 0x1,
 		}
 
+		/// <summary>
+		/// An enumeration of the kinds of locks supported by this class.
+		/// </summary>
 		internal enum LockKind {
+			/// <summary>
+			/// A lock that supports concurrently executing threads that hold this same lock type.
+			/// Holders of this lock may not obtain a <see cref="LockKind.Write"/> lock without first
+			/// releasing all their <see cref="LockKind.Read"/> locks.
+			/// </summary>
 			Read,
+
+			/// <summary>
+			/// A lock that may run concurrently with standard readers, but is exclusive of any other
+			/// upgradeable readers.  Holders of this lock are allowed to obtain a write lock while
+			/// holding this lock to guarantee continuity of state between what they read and what they write.
+			/// </summary>
 			UpgradeableRead,
+
+			/// <summary>
+			/// A mutually exclusive lock.
+			/// </summary>
 			Write,
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether the caller holds a read lock.
+		/// </summary>
+		/// <remarks>
+		/// This property returns <c>false</c> if any other lock type is held, unless
+		/// within that alternate lock type this lock is also nested.
+		/// </remarks>
 		public bool IsReadLockHeld {
 			get { return this.IsLockHeld(LockKind.Read); }
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether the caller holds an upgradeable read lock.
+		/// </summary>
+		/// <remarks>
+		/// This property returns <c>false</c> if any other lock type is held, unless
+		/// within that alternate lock type this lock is also nested.
+		/// </remarks>
 		public bool IsUpgradeableReadLockHeld {
 			get { return this.IsLockHeld(LockKind.UpgradeableRead); }
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether the caller holds a write lock.
+		/// </summary>
+		/// <remarks>
+		/// This property returns <c>false</c> if any other lock type is held, unless
+		/// within that alternate lock type this lock is also nested.
+		/// </remarks>
 		public bool IsWriteLockHeld {
 			get { return this.IsLockHeld(LockKind.Write); }
 		}
 
+		/// <summary>
+		/// Gets a task whose completion signals that this lock will no longer issue locks.
+		/// </summary>
+		/// <remarks>
+		/// This task only transitions to a complete state after a call to <see cref="Complete"/>.
+		/// </remarks>
 		public Task Completion {
 			get { return this.completionSource.Task; }
 		}
 
+		/// <summary>
+		/// Obtains a read lock, asynchronously awaiting for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>An awaitable object whose result is the lock releaser.</returns>
 		public Awaitable ReadLockAsync(CancellationToken cancellationToken = default(CancellationToken)) {
 			return new Awaitable(this, LockKind.Read, LockFlags.None, cancellationToken);
 		}
 
+		/// <summary>
+		/// Obtains an upgradeable read lock, asynchronously awaiting for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>An awaitable object whose result is the lock releaser.</returns>
 		public Awaitable UpgradeableReadLockAsync(CancellationToken cancellationToken = default(CancellationToken)) {
 			return new Awaitable(this, LockKind.UpgradeableRead, LockFlags.None, cancellationToken);
 		}
 
+		/// <summary>
+		/// Obtains a read lock, asynchronously awaiting for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="options">Modifications to normal lock behavior.</param>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>An awaitable object whose result is the lock releaser.</returns>
 		public Awaitable UpgradeableReadLockAsync(LockFlags options, CancellationToken cancellationToken = default(CancellationToken)) {
 			return new Awaitable(this, LockKind.UpgradeableRead, options, cancellationToken);
 		}
 
+		/// <summary>
+		/// Obtains a write lock, asynchronously awaiting for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>An awaitable object whose result is the lock releaser.</returns>
 		public Awaitable WriteLockAsync(CancellationToken cancellationToken = default(CancellationToken)) {
 			return new Awaitable(this, LockKind.Write, LockFlags.None, cancellationToken);
 		}
 
+		/// <summary>
+		/// Obtains a read lock, synchronously blocking for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>A lock releaser.</returns>
 		public Releaser ReadLock(CancellationToken cancellationToken = default(CancellationToken)) {
-			ThrowOnSta();
+			ThrowIfSta();
 			var awaiter = this.ReadLockAsync(cancellationToken).GetAwaiter();
 			return awaiter.GetResult();
 		}
 
+		/// <summary>
+		/// Obtains an upgradeable read lock, synchronously blocking for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>A lock releaser.</returns>
 		public Releaser UpgradeableReadLock(CancellationToken cancellationToken = default(CancellationToken)) {
-			ThrowOnSta();
+			ThrowIfSta();
 			var awaiter = this.UpgradeableReadLockAsync(cancellationToken).GetAwaiter();
 			return awaiter.GetResult();
 		}
 
+		/// <summary>
+		/// Obtains an upgradeable read lock, synchronously blocking for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="options">Modifications to normal lock behavior.</param>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>A lock releaser.</returns>
 		public Releaser UpgradeableReadLock(LockFlags options, CancellationToken cancellationToken = default(CancellationToken)) {
-			ThrowOnSta();
+			ThrowIfSta();
 			var awaiter = this.UpgradeableReadLockAsync(options, cancellationToken).GetAwaiter();
 			return awaiter.GetResult();
 		}
 
+		/// <summary>
+		/// Obtains a write lock, synchronously blocking for the lock if it is not immediately available.
+		/// </summary>
+		/// <param name="cancellationToken">A token whose cancellation indicates lost interest in obtaining the lock.</param>
+		/// <returns>A lock releaser.</returns>
 		public Releaser WriteLock(CancellationToken cancellationToken = default(CancellationToken)) {
-			ThrowOnSta();
+			ThrowIfSta();
 			var awaiter = this.WriteLockAsync(cancellationToken).GetAwaiter();
 			return awaiter.GetResult();
 		}
 
+		/// <summary>
+		/// Prevents use or visibility of the caller's lock(s) until the returned value is disposed.
+		/// </summary>
+		/// <returns>The value to dispose to restore lock visibility.</returns>
+		/// <remarks>
+		/// This can be used by a write lock holder that is about to fork execution to avoid
+		/// two threads simultaneously believing they hold the exclusive write lock.
+		/// The lock should be hidden just before kicking off the work and can be restored immediately
+		/// after kicking off the work.
+		/// </remarks>
 		public Suppression HideLocks() {
 			return new Suppression(this);
 		}
 
+		/// <summary>
+		/// Causes new top-level lock requests to be rejected and the <see cref="Completion"/> task to transition
+		/// to a completed state after any issued locks have been released.
+		/// </summary>
 		public void Complete() {
 			lock (this.syncObject) {
 				this.completeInvoked = true;
@@ -129,6 +308,19 @@
 			}
 		}
 
+		/// <summary>
+		/// Registers a callback to be invoked when the write lock held by the caller is fully released.
+		/// </summary>
+		/// <param name="action">The asynchronous delegate to invoke.</param>
+		/// <remarks>
+		/// This supports some scenarios VC++ has where change event handlers need to inspect changes,
+		/// or follow up with other changes to respond to earlier changes, at the conclusion of the lock.
+		/// This method is safe to call from within a previously registered callback, in which case the
+		/// registered callback will run when previously registered callbacks have completed execution.
+		/// If the write lock is released to an upgradeable read lock, these callbacks are fired synchronously
+		/// with respect to the writer who is releasing the lock.  Otherwise, the callbacks are invoked
+		/// asynchronously with respect to the releasing thread.
+		/// </remarks>
 		public void OnBeforeWriteLockReleased(Func<Task> action) {
 			lock (this.syncObject) {
 				if (!this.IsWriteLockHeld) {
@@ -139,12 +331,19 @@
 			}
 		}
 
-		private static void ThrowOnSta() {
+		/// <summary>
+		/// Throws an exception if called on an STA thread.
+		/// </summary>
+		private static void ThrowIfSta() {
 			if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA) {
 				throw new InvalidOperationException();
 			}
 		}
 
+		/// <summary>
+		/// Transitions the <see cref="Completion"/> task to a completed state
+		/// if appropriate.
+		/// </summary>
 		private void CompleteIfAppropriate() {
 			if (!Monitor.IsEntered(this.syncObject)) {
 				throw new Exception();
@@ -157,6 +356,13 @@
 			}
 		}
 
+		/// <summary>
+		/// Detects which lock types the given lock holder has (including all nested locks).
+		/// </summary>
+		/// <param name="awaiter">The most nested lock to be considered.</param>
+		/// <param name="read">Receives a value indicating whether a read lock is held.</param>
+		/// <param name="upgradeableRead">Receives a value indicating whether an upgradeable read lock is held.</param>
+		/// <param name="write">Receives a value indicating whether a write lock is held.</param>
 		private void AggregateLockStackKinds(Awaiter awaiter, out bool read, out bool upgradeableRead, out bool write) {
 			read = false;
 			upgradeableRead = false;
@@ -191,6 +397,11 @@
 			}
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether all issued locks are merely the top-level lock or nesting locks of the specified lock.
+		/// </summary>
+		/// <param name="awaiter">The most nested lock.</param>
+		/// <returns><c>true</c> if all issued locks are the specified lock or nesting locks of it.</returns>
 		private bool AllHeldLocksAreByThisStack(Awaiter awaiter) {
 			lock (this.syncObject) {
 				if (awaiter != null) {
@@ -209,6 +420,12 @@
 			}
 		}
 
+		/// <summary>
+		/// Gets a value indicating whether the specified lock is, or is a nested lock of, a given type.
+		/// </summary>
+		/// <param name="kind">The kind of lock being queried for.</param>
+		/// <param name="awaiter">The (possibly nested) lock.</param>
+		/// <returns><c>true</c> if the lock holder (also) holds the specified kind of lock.</returns>
 		private bool LockStackContains(LockKind kind, Awaiter awaiter) {
 			if (awaiter != null) {
 				lock (this.syncObject) {
@@ -232,6 +449,12 @@
 			return false;
 		}
 
+		/// <summary>
+		/// Checks whether the specified lock is an upgradeable read lock, with a <see cref="LockFlags.StickyWrite"/> flag,
+		/// which has actually be upgraded.
+		/// </summary>
+		/// <param name="awaiter">The lock to test.</param>
+		/// <returns><c>true</c> if the test succeeds; <c>false</c> otherwise.</returns>
 		private bool IsStickyWriteUpgradedLock(Awaiter awaiter) {
 			if (awaiter.Kind == LockKind.UpgradeableRead && (awaiter.Options & LockFlags.StickyWrite) == LockFlags.StickyWrite) {
 				lock (this.syncObject) {
@@ -242,6 +465,13 @@
 			return false;
 		}
 
+		/// <summary>
+		/// Checks whether the caller's held locks (or the specified lock stack) includes an active lock of the specified type.
+		/// Always <c>false</c> when called on an STA thread.
+		/// </summary>
+		/// <param name="kind">The type of lock to check for.</param>
+		/// <param name="awaiter">The most nested lock of the caller, or null to look up the caller's lock in the CallContext.</param>
+		/// <returns><c>true</c> if the caller holds active locks of the given type; <c>false</c> otherwise.</returns>
 		private bool IsLockHeld(LockKind kind, Awaiter awaiter = null) {
 			if (Thread.CurrentThread.GetApartmentState() != ApartmentState.STA) {
 				if (awaiter == null) {
@@ -258,7 +488,17 @@
 			return false;
 		}
 
+		/// <summary>
+		/// Checks whether a given lock is active. 
+		/// Always <c>false</c> when called on an STA thread.
+		/// </summary>
+		/// <param name="awaiter">The lock to check.</param>
+		/// <returns><c>true</c> if the lock is currently issued and the caller is not on an STA thread.</returns>
 		private bool IsLockActive(Awaiter awaiter) {
+			if (awaiter == null) {
+				throw new ArgumentNullException("awaiter");
+			}
+
 			if (Thread.CurrentThread.GetApartmentState() != ApartmentState.STA) {
 				lock (this.syncObject) {
 					return this.GetActiveLockSet(awaiter.Kind).Contains(awaiter);
@@ -268,6 +508,16 @@
 			return false;
 		}
 
+		/// <summary>
+		/// Immediately issues a lock to the specified awaiter if it is available.
+		/// </summary>
+		/// <param name="awaiter">The awaiter to issue a lock to.</param>
+		/// <param name="previouslyQueued">
+		/// A value indicating whether this lock was previously queued.  <c>false</c> if this is a new just received request.
+		/// The value is used to determine whether to reject it if <see cref="Complete"/> has already been called and this
+		/// is a new top-level request.
+		/// </param>
+		/// <returns>A value indicating whether the lock was issued.</returns>
 		private bool TryIssueLock(Awaiter awaiter, bool previouslyQueued) {
 			lock (this.syncObject) {
 				if (this.completeInvoked && !previouslyQueued) {
