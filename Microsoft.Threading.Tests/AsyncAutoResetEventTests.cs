f3415c6551974aaf825ae50d7699fc8478f5a0f8
Andrew Arnott <andrewarnott@gmail.com>
2012-06-23 12:26:49 -0700

    Added AsyncAutoResetEvent

---
+++ Microsoft.Threading.Tests/AsyncAutoResetEventTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Microsoft.Threading.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class AsyncAutoResetEventTests : TestBase {
+		private AsyncAutoResetEvent evt;
+
+		[TestInitialize]
+		public void Initialize() {
+			this.evt = new AsyncAutoResetEvent();
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task SingleThreadedPulse() {
+			for (int i = 0; i < 5; i++) {
+				var t = this.evt.WaitAsync();
+				Assert.IsFalse(t.IsCompleted);
+				this.evt.Set();
+				await t;
+			}
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task MultipleSetOnlySignalsOnce() {
+			this.evt.Set();
+			this.evt.Set();
+			await this.evt.WaitAsync();
+			var t = this.evt.WaitAsync();
+			Assert.IsFalse(t.IsCompleted);
+			await Task.Delay(AsyncDelay);
+			Assert.IsFalse(t.IsCompleted);
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task OrderPreservingQueue() {
+			var waiters = new Task[5];
+			for (int i = 0; i < waiters.Length; i++) {
+				waiters[i] = this.evt.WaitAsync();
+			}
+
+			for (int i = 0; i < waiters.Length; i++) {
+				this.evt.Set();
+				await waiters[i];
+			}
+		}
+	}
+}
---
+++ Microsoft.Threading/AsyncAutoResetEvent.cs
@@ -0,0 +1,61 @@
+﻿namespace Microsoft.Threading {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// An asynchronous implementation of an AutoResetEvent.
+	/// </summary>
+	public class AsyncAutoResetEvent {
+		/// <summary>
+		/// A task that is already completed.
+		/// </summary>
+		private readonly static Task completedTask = Task.FromResult(true);
+
+		/// <summary>
+		/// A queue of folks awaiting signals.
+		/// </summary>
+		private readonly Queue<TaskCompletionSource<bool>> signalAwaiters = new Queue<TaskCompletionSource<bool>>();
+
+		/// <summary>
+		/// A value indicating whether this event is already in a signaled state.
+		/// </summary>
+		private bool signaled;
+
+		/// <summary>
+		/// Returns an awaitable that may be used to asynchronously acquire the next signal.
+		/// </summary>
+		/// <returns>An awaitable.</returns>
+		public Task WaitAsync() {
+			lock (this.signalAwaiters) {
+				if (this.signaled) {
+					this.signaled = false;
+					return completedTask;
+				} else {
+					var tcs = new TaskCompletionSource<bool>();
+					this.signalAwaiters.Enqueue(tcs);
+					return tcs.Task;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Sets the signal if it has not already been set, allowing one awaiter to handle the signal if one is already waiting.
+		/// </summary>
+		public void Set() {
+			TaskCompletionSource<bool> toRelease = null;
+			lock (this.signalAwaiters) {
+				if (this.signalAwaiters.Count > 0) {
+					toRelease = this.signalAwaiters.Dequeue();
+				} else if (!this.signaled) {
+					this.signaled = true;
+				}
+			}
+			if (toRelease != null) {
+				toRelease.SetResult(true);
+			}
+		}
+	}
+}
