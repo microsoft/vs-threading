86d91fa3d17e6b32557336bb763d9b44df99c0a7
Andrew Arnott <andrewarnott@gmail.com>
2012-06-23 11:59:18 -0700

    Added AsyncManualResetEvent

---
+++ Microsoft.Threading.Tests/AsyncManualResetEventTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Microsoft.Threading.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class AsyncManualResetEventTests : TestBase {
+		private AsyncManualResetEvent evt;
+
+		[TestInitialize]
+		public void Initialize() {
+			this.evt = new AsyncManualResetEvent();
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public void CtorDefaultParameter() {
+			Assert.IsFalse(new System.Threading.ManualResetEventSlim().IsSet);
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public void DefaultSignaledState() {
+			Assert.IsTrue(new AsyncManualResetEvent(true).IsSet);
+			Assert.IsFalse(new AsyncManualResetEvent(false).IsSet);
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public void NonBlocking() {
+			this.evt.Set();
+			Assert.IsTrue(this.evt.WaitAsync().IsCompleted);
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task Blocking() {
+			this.evt.Reset();
+			var result = this.evt.WaitAsync();
+			Assert.IsFalse(result.IsCompleted);
+			this.evt.Set();
+			await result;
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task Reset() {
+			this.evt.Set();
+			this.evt.Reset();
+			var result = this.evt.WaitAsync();
+			Assert.IsFalse(result.IsCompleted);
+		}
+	}
+}
---
+++ Microsoft.Threading/AsyncManualResetEvent.cs
@@ -0,0 +1,68 @@
+﻿namespace Microsoft.Threading {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// A flavor of <see cref="ManualResetEvent"/> that can be asynchronously awaited on.
+	/// </summary>
+	public class AsyncManualResetEvent {
+		/// <summary>
+		/// The task to return from <see cref="WaitAsync"/>
+		/// </summary>
+		private volatile TaskCompletionSource<object> taskCompletionSource = new TaskCompletionSource<object>();
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AsyncManualResetEvent"/> class.
+		/// </summary>
+		/// <param name="initialState">A value indicating whether the event should be initially signaled.</param>
+		public AsyncManualResetEvent(bool initialState = false) {
+			if (initialState) {
+				this.taskCompletionSource.SetResult(false);
+			}
+		}
+
+		public bool IsSet {
+			get { return this.taskCompletionSource.Task.IsCompleted; }
+		}
+
+		/// <summary>
+		/// Returns a task that will be completed when this event is set.
+		/// </summary>
+		public Task WaitAsync() {
+			return this.taskCompletionSource.Task;
+		}
+
+		/// <summary>
+		/// Sets this event to unblock callers of <see cref="WaitAsync"/>.
+		/// </summary>
+		public void Set() {
+			var tcs = this.taskCompletionSource;
+			Task.Factory.StartNew(
+				s => ((TaskCompletionSource<object>)s).TrySetResult(true),
+				tcs,
+				CancellationToken.None,
+				TaskCreationOptions.PreferFairness,
+				TaskScheduler.Default);
+			tcs.Task.Wait();
+		}
+
+		/// <summary>
+		/// Resets this event to a state that will block callers of <see cref="WaitAsync"/>.
+		/// </summary>
+		public void Reset() {
+			while (true) {
+				var tcs = this.taskCompletionSource;
+#pragma warning disable 0420
+				if (!tcs.Task.IsCompleted ||
+					Interlocked.CompareExchange(ref this.taskCompletionSource, new TaskCompletionSource<object>(), tcs) == tcs) {
+					return;
+				}
+#pragma warning restore 0420
+			}
+		}
+	}
+}
