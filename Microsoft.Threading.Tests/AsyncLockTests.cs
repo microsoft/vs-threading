25355a4d1a7311558a574f2058c9418f4e7cd1c4
Andrew Arnott <andrewarnott@gmail.com>
2012-06-23 14:31:25 -0700

    Added AsyncLock

---
+++ Microsoft.Threading.Tests/AsyncLockTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Microsoft.Threading.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class AsyncLockTests : TestBase {
+		private AsyncLock lck = new AsyncLock();
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task Uncontested() {
+			using (await this.lck.LockAsync()) {
+			}
+
+			using (await this.lck.LockAsync()) {
+			}
+
+			using (await this.lck.LockAsync()) {
+			}
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task Contested() {
+			var first = this.lck.LockAsync();
+			Assert.IsTrue(first.IsCompleted);
+			var second = this.lck.LockAsync();
+			Assert.IsFalse(second.IsCompleted);
+			first.Result.Dispose();
+			await second;
+			second.Result.Dispose();
+		}
+
+		[TestMethod, Timeout(TestTimeout)]
+		public async Task ContestedAndCancelled() {
+			var cts = new CancellationTokenSource();
+			var first = this.lck.LockAsync();
+			var second = this.lck.LockAsync(cts.Token);
+			Assert.IsFalse(second.IsCompleted);
+			cts.Cancel();
+			first.Result.Dispose();
+			try {
+				await second;
+				Assert.Fail("Expected OperationCanceledException not thrown.");
+			} catch (OperationCanceledException) {
+			}
+		}
+	}
+}
---
+++ Microsoft.Threading/AsyncLock.cs
@@ -0,0 +1,80 @@
+﻿namespace Microsoft.Threading {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// An asynchronous <see cref="Monitor"/> like class.
+	/// </summary>
+	public class AsyncLock {
+		/// <summary>
+		/// The semaphore used to keep concurrent access to this lock to just 1.
+		/// </summary>
+		private readonly SemaphoreSlim semaphore;
+
+		/// <summary>
+		/// A task to return for any uncontested request for the lock.
+		/// </summary>
+		private readonly Task<Releaser> uncontestedReleaser;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AsyncLock"/> class.
+		/// </summary>
+		public AsyncLock() {
+			this.semaphore = new SemaphoreSlim(1);
+			this.uncontestedReleaser = Task.FromResult(new Releaser(this));
+		}
+
+		/// <summary>
+		/// Requests access to the lock.
+		/// </summary>
+		/// <param name="cancellationToken">A token whose cancellation signals lost interest in the lock.</param>
+		/// <returns>A task whose result is a releaser that should be disposed to release the lock.</returns>
+		public Task<Releaser> LockAsync(CancellationToken cancellationToken = default(CancellationToken)) {
+			var wait = this.semaphore.WaitAsync(cancellationToken);
+			return wait.IsCompleted
+				? this.uncontestedReleaser // uncontested lock
+				: wait.ContinueWith(
+					(waiter, state) => {
+						if (waiter.IsCanceled) {
+							throw new OperationCanceledException();
+						}
+
+						return new Releaser((AsyncLock)state);
+					},
+					this,
+					CancellationToken.None,
+					TaskContinuationOptions.ExecuteSynchronously,
+					TaskScheduler.Default);
+		}
+
+		/// <summary>
+		/// A value whose disposal triggers the release of a lock.
+		/// </summary>
+		public struct Releaser : IDisposable {
+			/// <summary>
+			/// The lock instance to release.
+			/// </summary>
+			private readonly AsyncLock toRelease;
+
+			/// <summary>
+			/// Initializes a new instance of the <see cref="Releaser"/> struct.
+			/// </summary>
+			/// <param name="toRelease">The lock instance to release on.</param>
+			internal Releaser(AsyncLock toRelease) {
+				this.toRelease = toRelease;
+			}
+
+			/// <summary>
+			/// Releases the lock.
+			/// </summary>
+			public void Dispose() {
+				if (this.toRelease != null)
+					this.toRelease.semaphore.Release();
+			}
+		}
+	}
+}
