4c3ff941cca8b7c2158f0dd4ba1b9e1224bfdc87
Andrew Arnott <andrewarnott@gmail.com>
2012-07-10 20:46:49 -0700

    Added AsyncPump class and tests.

---
+++ Microsoft.Threading.Tests/AsyncPumpTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Microsoft.Threading.Tests {
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	[TestClass]
+	public class AsyncPumpTests {
+		[TestMethod]
+		public void RunActionSTA() {
+			RunActionHelper();
+		}
+
+		[TestMethod]
+		public async Task RunActionMTA() {
+			await Task.Run(() => RunActionHelper());
+		}
+
+		[TestMethod]
+		public void RunFuncOfTaskSTA() {
+			RunFuncOfTaskHelper();
+		}
+
+		[TestMethod]
+		public async Task RunFuncOfTaskMTA() {
+			await Task.Run(() => RunFuncOfTaskHelper());
+		}
+
+		[TestMethod]
+		public void RunFuncOfTaskOfTSTA() {
+			RunFuncOfTaskOfTHelper();
+		}
+
+		[TestMethod]
+		public async Task RunFuncOfTaskOfTMTA() {
+			await Task.Run(() => RunFuncOfTaskOfTHelper());
+		}
+
+		private static void RunActionHelper() {
+			var initialThread = Thread.CurrentThread;
+			AsyncPump.Run((Action)async delegate {
+				Assert.AreSame(initialThread, Thread.CurrentThread);
+				await Task.Yield();
+				Assert.AreSame(initialThread, Thread.CurrentThread);
+			});
+		}
+
+		private static void RunFuncOfTaskHelper() {
+			var initialThread = Thread.CurrentThread;
+			AsyncPump.Run(async delegate {
+				Assert.AreSame(initialThread, Thread.CurrentThread);
+				await Task.Yield();
+				Assert.AreSame(initialThread, Thread.CurrentThread);
+			});
+		}
+
+		private static void RunFuncOfTaskOfTHelper() {
+			var initialThread = Thread.CurrentThread;
+			var expectedResult = new GenericParameterHelper();
+			GenericParameterHelper actualResult = AsyncPump.Run(async delegate {
+				Assert.AreSame(initialThread, Thread.CurrentThread);
+				await Task.Yield();
+				Assert.AreSame(initialThread, Thread.CurrentThread);
+				return expectedResult;
+			});
+			Assert.AreSame(expectedResult, actualResult);
+		}
+	}
+}
---
+++ Microsoft.Threading/AsyncPump.cs
@@ -0,0 +1,143 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AsyncPump.cs" company="Microsoft">
+//     Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Threading.Tests {
+	using System;
+	using System.Collections.Concurrent;
+	using System.Collections.Generic;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	/// <summary>Provides a pump that supports running asynchronous methods on the current thread.</summary>
+	public static class AsyncPump {
+		/// <summary>Runs the specified asynchronous method.</summary>
+		/// <param name="asyncMethod">The asynchronous method to execute.</param>
+		public static void Run(Action asyncMethod) {
+			Requires.NotNull(asyncMethod, "asyncMethod");
+
+			var prevCtx = SynchronizationContext.Current;
+			try {
+				// Establish the new context
+				var syncCtx = new SingleThreadSynchronizationContext();
+				SynchronizationContext.SetSynchronizationContext(syncCtx);
+
+				// Invoke the function
+				syncCtx.OperationStarted();
+				asyncMethod();
+				syncCtx.OperationCompleted();
+
+				// Pump continuations and propagate any exceptions
+				syncCtx.RunOnCurrentThread();
+			} finally {
+				SynchronizationContext.SetSynchronizationContext(prevCtx);
+			}
+		}
+
+		/// <summary>Runs the specified asynchronous method.</summary>
+		/// <param name="asyncMethod">The asynchronous method to execute.</param>
+		public static void Run(Func<Task> asyncMethod) {
+			Requires.NotNull(asyncMethod, "asyncMethod");
+
+			var prevCtx = SynchronizationContext.Current;
+			try {
+				// Establish the new context
+				var syncCtx = new SingleThreadSynchronizationContext();
+				SynchronizationContext.SetSynchronizationContext(syncCtx);
+
+				// Invoke the function and alert the context to when it completes
+				var t = asyncMethod();
+				Verify.Operation(t != null, "No task provided.");
+				t.ContinueWith(
+					(_, state) => ((SingleThreadSynchronizationContext)syncCtx).Complete(),
+					syncCtx,
+					TaskScheduler.Default);
+
+				// Pump continuations and propagate any exceptions
+				syncCtx.RunOnCurrentThread();
+				t.GetAwaiter().GetResult();
+			} finally {
+				SynchronizationContext.SetSynchronizationContext(prevCtx);
+			}
+		}
+
+		/// <summary>Runs the specified asynchronous method.</summary>
+		/// <param name="asyncMethod">The asynchronous method to execute.</param>
+		public static T Run<T>(Func<Task<T>> asyncMethod) {
+			Requires.NotNull(asyncMethod, "asyncMethod");
+
+			var prevCtx = SynchronizationContext.Current;
+			try {
+				// Establish the new context
+				var syncCtx = new SingleThreadSynchronizationContext();
+				SynchronizationContext.SetSynchronizationContext(syncCtx);
+
+				// Invoke the function and alert the context to when it completes
+				var t = asyncMethod();
+				Verify.Operation(t != null, "No task provided.");
+				t.ContinueWith(
+					(_, state) => ((SingleThreadSynchronizationContext)syncCtx).Complete(),
+					syncCtx,
+					TaskScheduler.Default);
+
+				// Pump continuations and propagate any exceptions
+				syncCtx.RunOnCurrentThread();
+				return t.GetAwaiter().GetResult();
+			} finally {
+				SynchronizationContext.SetSynchronizationContext(prevCtx);
+			}
+		}
+
+		/// <summary>Provides a SynchronizationContext that's single-threaded.</summary>
+		private sealed class SingleThreadSynchronizationContext : SynchronizationContext {
+			/// <summary>The queue of work items.</summary>
+			private readonly BlockingCollection<KeyValuePair<SendOrPostCallback, object>> queue =
+				new BlockingCollection<KeyValuePair<SendOrPostCallback, object>>();
+
+			/// <summary>The processing thread.</summary>
+			private readonly Thread thread = Thread.CurrentThread;
+
+			/// <summary>The number of outstanding operations.</summary>
+			private int operationCount = 0;
+
+			/// <summary>Dispatches an asynchronous message to the synchronization context.</summary>
+			/// <param name="d">The System.Threading.SendOrPostCallback delegate to call.</param>
+			/// <param name="state">The object passed to the delegate.</param>
+			public override void Post(SendOrPostCallback d, object state) {
+				Requires.NotNull(d, "d");
+				this.queue.Add(new KeyValuePair<SendOrPostCallback, object>(d, state));
+			}
+
+			/// <summary>Not supported.</summary>
+			public override void Send(SendOrPostCallback d, object state) {
+				throw new NotSupportedException("Synchronously sending is not supported.");
+			}
+
+			/// <summary>Runs an loop to process all queued work items.</summary>
+			public void RunOnCurrentThread() {
+				foreach (var workItem in this.queue.GetConsumingEnumerable()) {
+					workItem.Key(workItem.Value);
+				}
+			}
+
+			/// <summary>Notifies the context that no more work will arrive.</summary>
+			public void Complete() {
+				this.queue.CompleteAdding();
+			}
+
+			/// <summary>Invoked when an async operation is started.</summary>
+			public override void OperationStarted() {
+				Interlocked.Increment(ref this.operationCount);
+			}
+
+			/// <summary>Invoked when an async operation is completed.</summary>
+			public override void OperationCompleted() {
+				if (Interlocked.Decrement(ref this.operationCount) == 0) {
+					this.Complete();
+				}
+			}
+		}
+	}
+}
