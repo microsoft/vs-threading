0364901f73a62b509bd371a279fd6026d06731c8
Andrew Arnott <andarno@microsoft.com>
2012-07-31 14:49:34 -0700

    Revert "Removed unused code."
    This reverts commit 0dfa24abd6d87da5be039a31f19820bdf1e05bff.

--- Microsoft.Threading/AsyncPump.cs
+++ Microsoft.Threading/AsyncPump.cs
@@ -301,6 +301,19 @@ namespace Microsoft.Threading {
 		/// <summary>
 		/// Schedules the specified delegate for execution on the Main thread.
 		/// </summary>
+		/// <param name="callback">The delegate to invoke.</param>
+		/// <param name="state">The argument to pass to the delegate.</param>
+		private void Post(SendOrPostCallback callback, object state) {
+			if (this.underlyingSynchronizationContext != null) {
+				this.Post(new SingleExecuteProtector(this, callback, state));
+			} else {
+				ThreadPool.QueueUserWorkItem(new WaitCallback(callback), state);
+			}
+		}
+
+		/// <summary>
+		/// Schedules the specified delegate for execution on the Main thread.
+		/// </summary>
 		/// <param name="wrapper">The delegate wrapper that guarantees the delegate cannot be invoked more than once.</param>
 		private void Post(SingleExecuteProtector wrapper) {
 			Assumes.NotNull(this.underlyingSynchronizationContext);
@@ -386,26 +399,49 @@ namespace Microsoft.Threading {
 			/// The delegate to invoke.  <c>null</c> if it has already been invoked.
 			/// </summary>
 			/// <value>May be of type <see cref="Action"/> or <see cref="SendOrPostCallback"/>.</value>
-			private Action invokeDelegate;
+			private object invokeDelegate;
+
+			/// <summary>
+			/// The value to pass to the delegate if it is a <see cref="SendOrPostCallback"/>.
+			/// </summary>
+			private object state;
+
+			/// <summary>
+			/// Initializes a new instance of the <see cref="SingleExecuteProtector"/> class.
+			/// </summary>
+			/// <param name="asyncPump">The <see cref="AsyncPump"/> instance that created this.</param>
+			private SingleExecuteProtector(AsyncPump asyncPump) {
+				Requires.NotNull(asyncPump, "asyncPump");
+				this.asyncPump = asyncPump;
+			}
 
 			/// <summary>
 			/// Initializes a new instance of the <see cref="SingleExecuteProtector"/> class.
 			/// </summary>
 			/// <param name="asyncPump">The <see cref="AsyncPump"/> instance that created this.</param>
 			/// <param name="action">The delegate being wrapped.</param>
-			internal SingleExecuteProtector(AsyncPump asyncPump, Action action) {
-				Requires.NotNull(asyncPump, "asyncPump");
-				Requires.NotNull(action, "action");
-
-				this.asyncPump = asyncPump;
+			internal SingleExecuteProtector(AsyncPump asyncPump, Action action)
+				: this(asyncPump) {
 				this.invokeDelegate = action;
+			}
+
+			/// <summary>
+			/// Initializes a new instance of the <see cref="SingleExecuteProtector"/> class.
+			/// </summary>
+			/// <param name="asyncPump">The <see cref="AsyncPump"/> instance that created this.</param>
+			/// <param name="callback">The delegate being wrapped.</param>
+			/// <param name="state">The value to pass to the delegate.</param>
+			internal SingleExecuteProtector(AsyncPump asyncPump, SendOrPostCallback callback, object state)
+				: this(asyncPump) {
+				this.invokeDelegate = callback;
+				this.state = state;
 			}
 
 			/// <summary>
 			/// Executes the delegate if it has not already executed.
 			/// </summary>
 			internal bool TryExecute() {
-				var invokeDelegate = Interlocked.Exchange(ref this.invokeDelegate, null);
+				object invokeDelegate = Interlocked.Exchange(ref this.invokeDelegate, null);
 				if (invokeDelegate != null) {
 					lock (this.asyncPump.syncObject) {
 						// This work item will usually be at the head of the queue, but since the
@@ -413,7 +449,16 @@ namespace Microsoft.Threading {
 						this.asyncPump.pendingActions.RemoveMidQueue(this);
 					}
 
-					invokeDelegate();
+					var action = invokeDelegate as Action;
+					if (action != null) {
+						action();
+					} else {
+						var callback = invokeDelegate as SendOrPostCallback;
+						Assumes.NotNull(callback);
+						callback(this.state);
+						this.state = null;
+					}
+
 					return true;
 				} else {
 					return false;
