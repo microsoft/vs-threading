c82fa1e55f68c65993b7ab1e1d345a96cc6b9d23
Andrew Arnott <andarno@microsoft.com>
2012-08-24 11:13:26 -0700

    Added AsyncLazy test that demonstrates deadlock due to missing AsyncPump coordination.

---
+++ Microsoft.Threading.Tests/AsyncLazyAsyncPumpTests.cs
@@ -0,0 +1,62 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AsyncLazyAsyncPumpTests.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Threading.Tests {
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+	using System.Windows.Threading;
+
+	[TestClass]
+	public class AsyncLazyAsyncPumpTests : TestBase {
+		private AsyncPump asyncPump;
+		private Thread originalThread;
+
+		[TestInitialize]
+		public void Initialize() {
+			var ctxt = new DispatcherSynchronizationContext();
+			SynchronizationContext.SetSynchronizationContext(ctxt);
+			this.asyncPump = new AsyncPump();
+			this.originalThread = Thread.CurrentThread;
+		}
+
+		/// <summary>
+		/// Verifies that even after the value factory has been invoked
+		/// its dependency on the Main thread can be satisfied by
+		/// someone synchronously blocking on the Main thread that is
+		/// also interested in its value.
+		/// </summary>
+		[TestMethod, Timeout(TestTimeout)]
+		public void AsyncPumpAsyncLazy() {
+			var lazy = new AsyncLazy<object>(async delegate {
+				await Task.Yield();
+				return new object();
+			});
+
+			var resultTask = lazy.GetValueAsync();
+			Assert.IsFalse(resultTask.IsCompleted); // valueFactory needs this thread to complete.
+
+			this.asyncPump.RunSynchronously(async delegate {
+				// The interesting bit we're testing here is that
+				// the value factory has already been invoked.  It cannot
+				// complete until the Main thread is available and we're blocking
+				// the Main thread waiting for it to complete.
+				// This will deadlock unless the AsyncLazy joins
+				// the value factory's async pump with the currently blocking one.
+				var value = await lazy.GetValueAsync();
+				Assert.IsNotNull(value);
+			});
+
+			// Now that the value factory has completed, the earlier acquired
+			// task should have no problem completing.
+			Assert.IsTrue(resultTask.Wait(AsyncDelay));
+		}
+	}
+}
