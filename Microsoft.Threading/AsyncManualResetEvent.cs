microsoft/vs-threading

commit e7e17d10be189647c76f0b6da4d7d0ddfe3f8331

Adds EmptyStruct to save a few bytes with each TaskCompletionSource<T> allocation.

Microsoft.Threading/AsyncManualResetEvent.cs
@@ -16,7 +16,7 @@
 		/// <summary>
 		/// The task to return from <see cref="WaitAsync"/>
 		/// </summary>
-		private volatile TaskCompletionSource<object> taskCompletionSource = new TaskCompletionSource<object>();
+		private volatile TaskCompletionSource<EmptyStruct> taskCompletionSource = new TaskCompletionSource<EmptyStruct>();
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="AsyncManualResetEvent"/> class.
@@ -24,7 +24,7 @@
 		/// <param name="initialState">A value indicating whether the event should be initially signaled.</param>
 		public AsyncManualResetEvent(bool initialState = false) {
 			if (initialState) {
-				this.taskCompletionSource.SetResult(false);
+				this.taskCompletionSource.SetResult(EmptyStruct.Instance);
 			}
 		}
 
@@ -64,7 +64,7 @@
 				var tcs = this.taskCompletionSource;
 #pragma warning disable 0420
 				if (!tcs.Task.IsCompleted ||
-					Interlocked.CompareExchange(ref this.taskCompletionSource, new TaskCompletionSource<object>(), tcs) == tcs) {
+					Interlocked.CompareExchange(ref this.taskCompletionSource, new TaskCompletionSource<EmptyStruct>(), tcs) == tcs) {
 					return;
 				}
 #pragma warning restore 0420

Microsoft.Threading/EmptyStruct.cs
@@ -1,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EmptyStruct.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Threading
+{
+    /// <summary>
+    /// An empty struct.
+    /// </summary>
+    /// <remarks>
+    /// This can save 4 bytes over System.Object when a type argument is required for a generic type, but entirely unused.
+    /// </remarks>
+    internal struct EmptyStruct
+    {
+        /// <summary>
+        /// Gets an instance of the empty struct.
+        /// </summary>
+        internal static EmptyStruct Instance
+        {
+            get { return new EmptyStruct(); }
+        }
+    }
+}

Microsoft.Threading/Microsoft.Threading.csproj
@@ -54,6 +54,7 @@
     <Compile Include="AsyncReaderWriterLock.cs" />
     <Compile Include="AsyncReaderWriterResourceLock.cs" />
     <Compile Include="AwaitExtensions.cs" />
+    <Compile Include="EmptyStruct.cs" />
     <Compile Include="EnumerateOneOrMany.cs" />
     <Compile Include="IAsyncDisposable.cs" />
     <Compile Include="InternalUtilities.cs" />

commit 3f2e06a791a7b97396425be95336c9846267c79c

Fixes regression introduced in e7e17d10be.

Microsoft.Threading/AsyncManualResetEvent.cs
@@ -48,7 +48,7 @@
 		public void Set() {
 			var tcs = this.taskCompletionSource;
 			Task.Factory.StartNew(
-				s => ((TaskCompletionSource<object>)s).TrySetResult(true),
+				s => ((TaskCompletionSource<EmptyStruct>)s).TrySetResult(EmptyStruct.Instance),
 				tcs,
 				CancellationToken.None,
 				TaskCreationOptions.PreferFairness,