47d34acf21b8b1a56f2bcf7028ad247de19cfa5b
Andrew Arnott <andarno@microsoft.com>
2013-01-22 13:03:48 -0800

    Defines hang report contribution interfaces.

---
+++ Microsoft.Threading/IHangReportContributor.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IHangReportContributor.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Threading {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// Provides a facility to produce reports that may be useful when analyzing hangs.
+	/// </summary>
+	public interface IHangReportContributor {
+		/// <summary>
+		/// Contributes data for a hang report.
+		/// </summary>
+		/// <returns>The hang report contribution. Null values should be ignored.</returns>
+		HangReportContribution GetHangReport();
+	}
+}
