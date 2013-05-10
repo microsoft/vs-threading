49c5dee986f47082db67b14158ca2595f34673c1
Andrew Arnott <andrewarnott@gmail.com>
2013-05-10 15:52:39 -0700

    Xml doc comment improvements

--- Microsoft.VisualStudio.Threading/NoMessagePumpSyncContext.cs
+++ Microsoft.VisualStudio.Threading/NoMessagePumpSyncContext.cs
@@ -36,6 +36,12 @@ namespace Microsoft.VisualStudio.Threading {
 		/// <summary>
 		/// Synchronously blocks without a message pump.
 		/// </summary>
+		/// <param name="waitHandles">An array of type <see cref="T:System.IntPtr" /> that contains the native operating system handles.</param>
+		/// <param name="waitAll">true to wait for all handles; false to wait for any handle.</param>
+		/// <param name="millisecondsTimeout">The number of milliseconds to wait, or <see cref="F:System.Threading.Timeout.Infinite" /> (-1) to wait indefinitely.</param>
+		/// <returns>
+		/// The array index of the object that satisfied the wait.
+		/// </returns>
 		public override int Wait(IntPtr[] waitHandles, bool waitAll, int millisecondsTimeout) {
 			return NativeMethods.WaitForMultipleObjects((uint)waitHandles.Length, waitHandles, waitAll, (uint)millisecondsTimeout);
 		}
