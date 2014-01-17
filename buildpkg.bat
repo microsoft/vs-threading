c78256c67e1f93de804b6704014cf3e01e591499
Andrew Arnott <andarno@microsoft.com>
2014-01-16 16:59:27 -0800

    Updates nuget packaging scripts.

--- buildpkg.bat
+++ buildpkg.bat
@@ -5,8 +5,8 @@ if "%1"=="" (
     EXIT /b 1
 )
 
-msbuild "%~dp0Microsoft.Threading\Microsoft.Threading.csproj" /p:Configuration=Release	/v:minimal /nologo
+msbuild "%~dp0Microsoft.VisualStudio.Threading\Microsoft.VisualStudioThreading.csproj" /p:Configuration=Release /v:minimal /nologo
 
 @echo on
-nuget pack "%~dp0Microsoft.Threading.nuspec" -symbols -OutputDirectory Microsoft.Threading\bin -Version %1
-echo Package built: "%~dp0Microsoft.Threading\bin\Microsoft.Threading.%1.nupkg"
+nuget pack "%~dp0Microsoft.Threading.nuspec" -symbols -OutputDirectory Microsoft.VisualStudio.Threading\bin -Version %1
+echo Package built: "%~dp0Microsoft.VisualStudio.Threading\bin\Microsoft.Threading.%1.nupkg"
