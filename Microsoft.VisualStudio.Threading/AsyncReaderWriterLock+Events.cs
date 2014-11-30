bcd49f4a0e323c38cb54b8487fae0f64d115e625
Andrew Arnott <andarno@microsoft.com>
2014-11-29 21:39:15 -0800

    Refine the ETW event format and show off new techniques.

--- Microsoft.VisualStudio.Threading/AsyncReaderWriterLock+Events.cs
+++ Microsoft.VisualStudio.Threading/AsyncReaderWriterLock+Events.cs
@@ -5,16 +5,17 @@
 *********************************************************/
 
 namespace Microsoft.VisualStudio.Threading {
+	using System;
 	using System.Diagnostics.Tracing;
 
 	/// <content>
-	/// The <see cref="AsyncReaderWriterLock.Etw"/> class.
+	/// The <see cref="AsyncReaderWriterLock.Events"/> class.
 	/// </content>
 	partial class AsyncReaderWriterLock {
 		/// <summary>
 		/// The instance of the ETW logger to use.
 		/// </summary>
-		private static AsyncReaderWriterLockEvents Etw = AsyncReaderWriterLockEvents.Log;
+		private static Events Etw = Events.Log;
 
 		/// <summary>
 		/// The ETW source for logging events for the <see cref="AsyncReaderWriterLock"/>.
@@ -23,40 +24,83 @@ namespace Microsoft.VisualStudio.Threading {
 		/// We use a fully-descriptive type name because the type name becomes the name
 		/// of the ETW Provider.
 		/// </remarks>
-		internal sealed class AsyncReaderWriterLockEvents : EventSource {
+		[EventSource(Name = "AsyncReaderWriterLock")]
+		internal sealed class Events : EventSource {
 			/// <summary>
-			/// The event ID for the <see cref="Issued(int, int, int)"/> event.
+			/// The event ID for the <see cref="Issued(LockKind, int, int)"/> event.
 			/// </summary>
 			private const int IssuedLockCountsEvent = 1;
 
 			/// <summary>
-			/// The event ID for the <see cref="Waiting(int, int, int)"/> event.
+			/// The event ID for the <see cref="Waiting(LockKind, int, int, int)"/> event.
 			/// </summary>
 			private const int WaitingLockCountsEvent = 2;
 
 			/// <summary>
 			/// The singleton instance used for logging.
 			/// </summary>
-			internal static readonly AsyncReaderWriterLockEvents Log = new AsyncReaderWriterLockEvents();
+			internal static readonly Events Log = new Events();
 
 			/// <summary>
-			/// Initializes a new instance of the <see cref="AsyncReaderWriterLockEvents"/> class.
+			/// Initializes a new instance of the <see cref="Events"/> class.
 			/// </summary>
-			protected AsyncReaderWriterLockEvents() : base(false) {
+			protected Events() : base(false) {
 			}
 
 			/// <summary>
-			/// Logs the number of issued locks of each type.
+			/// Logs an issued lock.
 			/// </summary>
-			public void Issued(int issuedWriteCount, int issuedUpgradeableReadCount, int issuedReadCount) {
-				WriteEvent(IssuedLockCountsEvent, issuedWriteCount, issuedUpgradeableReadCount, issuedReadCount);
+			[Event(IssuedLockCountsEvent, Version = 1, Task = Tasks.LockRequest, Opcode = Opcodes.Issued,
+			Level = EventLevel.Informational, Keywords = Keywords.SomeKeyword)]
+			public void Issued(LockKind kind, int issuedUpgradeableReadCount, int issuedReadCount) {
+				WriteEvent(IssuedLockCountsEvent, (int)kind, issuedUpgradeableReadCount, issuedReadCount);
 			}
 
 			/// <summary>
-			/// Logs the number of waiting locks of each type.
+			/// Logs a wait for a lock.
 			/// </summary>
-			public void Waiting(int waitingWriteCount, int waitingUpgradeableReadCount, int waitingReadCount) {
-				WriteEvent(WaitingLockCountsEvent, waitingWriteCount, waitingUpgradeableReadCount, waitingReadCount);
+			[Event(WaitingLockCountsEvent, Version = 1, Task = Tasks.LockRequest, Opcode = Opcodes.Waiting,
+				Level = EventLevel.Informational, Keywords = Keywords.SomeKeyword)]
+			public void Waiting(LockKind kind, int issuedWriteCount, int issuedUpgradeableReadCount, int issuedReadCount) {
+				unsafe
+				{
+					EventData* eventPayload = stackalloc EventData[4];
+					eventPayload[0].Size = 4;
+					eventPayload[0].DataPointer = (IntPtr)(&kind);
+					eventPayload[1].Size = 4;
+					eventPayload[1].DataPointer = (IntPtr)(&issuedWriteCount);
+					eventPayload[2].Size = 4;
+					eventPayload[2].DataPointer = (IntPtr)(&issuedUpgradeableReadCount);
+					eventPayload[3].Size = 4;
+					eventPayload[3].DataPointer = (IntPtr)(&issuedReadCount);
+					WriteEventCore(WaitingLockCountsEvent, 4, eventPayload);
+				}
+			}
+
+			/// <summary>
+			/// The names of constants in this class make up the middle term in 
+			/// the AsyncReaderWriterLock/LockRequest/Issued event name.
+			/// </summary>
+			/// <remarks>The name of this class is important for EventSource.</remarks>
+			public class Tasks {
+				public const EventTask LockRequest = (EventTask)1;
+			}
+
+			/// <summary>I don't know what this is for yet.</summary>
+			/// <remarks>The name of this class is important for EventSource.</remarks>
+			public class Keywords {
+				public const EventKeywords SomeKeyword = (EventKeywords)1;
+			}
+
+			/// <summary>
+			/// The names of constants in this class make up the last term in 
+			/// the AsyncReaderWriterLock/LockRequest/Issued event name.
+			/// </summary>
+			/// <remarks>The name of this class is important for EventSource.</remarks>
+			public class Opcodes {
+				// Custom opcodes should range 11 - 239; see http://msdn.microsoft.com/en-us/library/windows/desktop/dd996918(v=vs.85).aspx
+				public const EventOpcode Waiting = (EventOpcode)100;
+				public const EventOpcode Issued = (EventOpcode)101;
 			}
 		}
 	}
