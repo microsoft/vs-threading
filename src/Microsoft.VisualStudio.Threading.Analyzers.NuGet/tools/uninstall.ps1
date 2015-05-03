3f92d86c3eb10fc688d706d39afef6af66ab1c5e
Andrew Arnott <andarno@microsoft.com>
2015-05-02 18:20:00 -0700

    Fix Analyzer NuGet to ship just the one assembly, and install as analyzer

---
+++ src/Microsoft.VisualStudio.Threading.Analyzers.NuGet/tools/uninstall.ps1
@@ -0,0 +1,7 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$p = Get-Project
+
+$analyzerFilePath = join-path $toolsPath "net451\Microsoft.VisualStudio.Threading.Analyzers.dll"
+
+$p.Object.AnalyzerReferences.Remove("$analyzerFilePath")
