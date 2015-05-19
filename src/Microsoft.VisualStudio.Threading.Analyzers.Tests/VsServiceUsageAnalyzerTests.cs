91e85cb54ef059431948d3850967b906cc546e91
Andrew Arnott <andarno@microsoft.com>
2015-05-18 20:42:39 -0700

    Skip another test that fails due to the Roslyn bug

--- src/Microsoft.VisualStudio.Threading.Analyzers.Tests/VsServiceUsageAnalyzerTests.cs
+++ src/Microsoft.VisualStudio.Threading.Analyzers.Tests/VsServiceUsageAnalyzerTests.cs
@@ -97,7 +97,7 @@ class Test {
             VerifyCSharpDiagnostic(test);
         }
 
-        [TestMethod]
+        [TestMethod, Ignore] // Fails due to Roslyn bug https://github.com/dotnet/roslyn/issues/2894
         public void InvokeVsSolutionAfterSwitchedToMainThreadAsync()
         {
             var test = @"
