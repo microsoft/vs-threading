eeb477ae2fc91f8e3a7356d9230c5abc980b968e
Andrew Arnott <andarno@microsoft.com>
2015-05-24 18:58:57 -0700

    Add comments to ThreadPool portable class

--- src/Microsoft.VisualStudio.Threading.Portable/ThreadPool.cs
+++ src/Microsoft.VisualStudio.Threading.Portable/ThreadPool.cs
@@ -16,13 +16,25 @@ namespace Microsoft.VisualStudio.Threading
     /// </summary>
     internal static class ThreadPool
     {
+        /// <summary>
+        /// Schedules a delegate for execution on a threadpool thread.
+        /// </summary>
+        /// <param name="action">The delegate to execute.</param>
         internal static void QueueUserWorkItem(Action action)
         {
             Task.Run(action);
         }
 
+        /// <summary>
+        /// Schedules a delegate for execution on a threadpool thread.
+        /// </summary>
+        /// <param name="action">The delegate to execute.</param>
+        /// <param name="state">A state object to pass to <paramref name="action"/>.</param>
         internal static void QueueUserWorkItem(Action<object> action, object state)
         {
+            // It is important to call the overload of StartNew that takes a TaskScheduler explicitly,
+            // since otherwise the default is TaskScheduler.Current, which has an undefined value
+            // as it depends on our caller, who we cannot predict.
             Task.Factory.StartNew(action, state, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default);
         }
     }
