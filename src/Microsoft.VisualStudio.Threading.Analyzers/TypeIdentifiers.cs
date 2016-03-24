3ad5ae7190751369a3884440cfe3d3a3ebea3efd
Adrian Vinca <adrianv@microsoft.com>
2016-03-24 14:11:16 -0700

    Removing unneeded dependency on Threading as analyzer fails to locate it because it is no longer in the GAC

---
+++ src/Microsoft.VisualStudio.Threading.Analyzers/TypeIdentifiers.cs
@@ -0,0 +1,32 @@
+﻿/********************************************************
+*                                                        *
+*   © Copyright (C) Microsoft. All rights reserved.      *
+*                                                        *
+*********************************************************/
+
+namespace Microsoft.VisualStudio.Threading.Analyzers
+{
+    /// <summary>
+    /// Identifiers used to identify various types so that we can avoid adding dependency only if absolutely needed.
+    /// <devremarks>For each predefine value here, please update the unit test to detect if values go out of sync with the real types they represent.</devremarks>
+    /// </summary>
+    public static class TypeIdentifiers
+    {
+        public static class TplExtensions
+        {
+            public const string FullName = "Microsoft.VisualStudio.Threading.TplExtensions";
+
+            public const string InvokeAsyncName = "InvokeAsync";
+        }
+
+        public static class AsyncEventHandler
+        {
+            public const string FullName = "Microsoft.VisualStudio.Threading.AsyncEventHandler";
+        }
+
+        public static class JoinableTaskFactory
+        {
+            public const string SwitchToMainThreadAsyncName = "SwitchToMainThreadAsync";
+        }
+    }
+}
