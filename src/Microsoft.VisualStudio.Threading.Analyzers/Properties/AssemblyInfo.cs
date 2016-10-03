a7e4fa39460b2a06c9faa2c0c338fd3d71640b56
Andrew Arnott <Andrew.Arnott@microsoft.com>
2016-10-03 16:04:02 -0700

    Fix satellite assembly signing

--- src/Microsoft.VisualStudio.Threading.Analyzers/Properties/AssemblyInfo.cs
+++ src/Microsoft.VisualStudio.Threading.Analyzers/Properties/AssemblyInfo.cs
@@ -6,8 +6,13 @@
 
 using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 [assembly: AssemblyTitle("Microsoft.VisualStudio.Threading.Analyzers")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyProduct("Microsoft® Visual Studio® Validation")]
+
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
--- src/Microsoft.VisualStudio.Threading.Shared/AssemblyInfo.cs
+++ src/Microsoft.VisualStudio.Threading.Shared/AssemblyInfo.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: AssemblyTitle("Microsoft.VisualStudio.Threading")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyProduct("Microsoft® Visual Studio® Validation")]
