149b151e6def8ad97e529a94f34981a55c9a85a9
Andrew Arnott <andrewarnott@gmail.com>
2016-12-09 22:14:55 -0800

    Abbreviate actual diagnostics printout

--- src/Microsoft.VisualStudio.Threading.Analyzers.Tests/Helpers/DiagnosticVerifier.cs
+++ src/Microsoft.VisualStudio.Threading.Analyzers.Tests/Helpers/DiagnosticVerifier.cs
@@ -298,48 +298,7 @@ namespace Microsoft.VisualStudio.Threading.Analyzers.Tests
             var builder = new StringBuilder();
             for (int i = 0; i < diagnostics.Length; ++i)
             {
-                builder.AppendLine("// " + diagnostics[i].ToString());
-
-                foreach (var analyzer in analyzers)
-                {
-                    var analyzerType = analyzer.GetType();
-                    var rules = analyzer.SupportedDiagnostics;
-
-                    foreach (var rule in rules)
-                    {
-                        if (rule != null && rule.Id == diagnostics[i].Id)
-                        {
-                            var location = diagnostics[i].Location;
-                            if (location == Location.None)
-                            {
-                                builder.AppendFormat("GetGlobalResult({0}.{1})", analyzerType.Name, rule.Id);
-                            }
-                            else
-                            {
-                                Assert.True(location.IsInSource,
-                                    string.Format("Test base does not currently handle diagnostics in metadata locations. Diagnostic in metadata:\r\n", diagnostics[i]));
-
-                                string resultMethodName = diagnostics[i].Location.SourceTree.FilePath.EndsWith(".cs") ? "GetCSharpResultAt" : "GetBasicResultAt";
-                                var linePosition = diagnostics[i].Location.GetLineSpan().StartLinePosition;
-
-                                builder.AppendFormat("{0}({1}, {2}, {3}.{4})",
-                                    resultMethodName,
-                                    linePosition.Line + 1,
-                                    linePosition.Character + 1,
-                                    analyzerType.Name,
-                                    rule.Id);
-                            }
-
-                            if (i != diagnostics.Length - 1)
-                            {
-                                builder.Append(',');
-                            }
-
-                            builder.AppendLine();
-                            break;
-                        }
-                    }
-                }
+                builder.AppendLine(diagnostics[i].ToString());
             }
 
             return builder.ToString();
