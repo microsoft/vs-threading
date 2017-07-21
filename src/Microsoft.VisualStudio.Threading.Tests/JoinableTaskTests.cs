c9ec1f74d9b65aff1b23f0ea197bef66d4d660ff
Andrew Arnott <andarno@microsoft.com>
2017-07-21 11:42:53 -0700

    Increase GC pressure test tolerance

--- src/Microsoft.VisualStudio.Threading.Tests/JoinableTaskTests.cs
+++ src/Microsoft.VisualStudio.Threading.Tests/JoinableTaskTests.cs
@@ -2838,7 +2838,7 @@
                         await TaskScheduler.Default;
                         await this.asyncPump.SwitchToMainThreadAsync(CancellationToken.None);
                     },
-                    3421);
+                    3423);
             }
         }
 
