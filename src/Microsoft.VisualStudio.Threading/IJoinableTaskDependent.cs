3a7627e869d7dcff91b08528f972a08035b0bba4
Lifeng Lu <lulifeng@hotmail.com>
2018-10-16 14:59:33 -0700

    Fix O(n^2) algorithm in JTF where JoinableTaskCollection is joined (#403)
    * prevent unnecessary allocations.
    
    * Refactory the dependency tracking data structure
    
    Pull JoinableTaskDependentNode out of JTF, so JTF collection can be treated as a different
    kind of nodes, just like a normal JTF.
    
    This is a messy change, because of all the visibility issue, and we cannot expose any API to external code, which eliminate the possibility of using 'protected' members.  I am not happy with many internal methods, but need get things to the working state first.
    
    * Minor code cleanups.
    
    * Add a lock check.
    
    * Refactorying to reduce type coupling.
    
    * Simple renaming.
    
    * Code cleanups
    
    Seal JoinableTaskDependentNode better.
    
    * Minor cleanups.
    
    * fix struct/class issues.
    
    * Renaming and update comments
    
    * Further code cleanups.
    
    * Step 1: remove base class.
    
    * Renaming JoinableTaskDependentNode to DependentData
    
    * Cleanup code, moving DependentData inside a static class
    wrap external calls.
    
    * Remove private properties to prepare removing thisDependent field.
    
    * Remove the data to hold thisDependent to the structure, and pass it in for every funciton call.
    
    * Fix styleCop
    
    * Fix earlier mistakes in refactorying.
    
    * remove unnecessary parameters.
    
    * Change methods requiring context to static
    
    This prevents mistakes to pass mismatching items.
    
    * Fix a mistake when refactorying code.

---
+++ src/Microsoft.VisualStudio.Threading/IJoinableTaskDependent.cs
@@ -0,0 +1,49 @@
+﻿/********************************************************
+*                                                        *
+*   © Copyright (C) Microsoft. All rights reserved.      *
+*                                                        *
+*********************************************************/
+
+namespace Microsoft.VisualStudio.Threading
+{
+    /// <summary>
+    /// Represents a dependent item in the JoinableTask dependency graph, it can be either a <see cref="JoinableTask"/> or a <see cref="JoinableTaskCollection"/>
+    /// </summary>
+    internal interface IJoinableTaskDependent
+    {
+        /// <summary>
+        /// Gets the <see cref="Threading.JoinableTaskContext"/> this node belongs to.
+        /// </summary>
+        JoinableTaskContext JoinableTaskContext { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether we need reference count child dependent node.  This is to keep the current behavior of <see cref="JoinableTaskCollection"/>.
+        /// </summary>
+        bool NeedRefCountChildDependencies { get; }
+
+        /// <summary>
+        /// Get the reference of dependent node to record dependencies.
+        /// </summary>
+        ref JoinableTaskDependencyGraph.JoinableTaskDependentData GetJoinableTaskDependentData();
+
+        /// <summary>
+        /// A function is called, when this dependent node is added to be a dependency of a parent node.
+        /// </summary>
+        void OnAddedToDependency(IJoinableTaskDependent parent);
+
+        /// <summary>
+        /// A function is called, when this dependent node is removed as a dependency of a parent node.
+        /// </summary>
+        void OnRemovedFromDependency(IJoinableTaskDependent parentNode);
+
+        /// <summary>
+        /// A function is called, when a dependent child is added.
+        /// </summary>
+        void OnDependencyAdded(IJoinableTaskDependent joinChild);
+
+        /// <summary>
+        /// A function is called, when a dependent child is removed.
+        /// </summary>
+        void OnDependencyRemoved(IJoinableTaskDependent joinChild);
+    }
+}
--- src/Microsoft.VisualStudio.Threading/JoinableTaskContext.cs
+++ src/Microsoft.VisualStudio.Threading/JoinableTaskContext.cs
@@ -323,7 +323,7 @@ namespace Microsoft.VisualStudio.Threading
             var ambientTask = this.AmbientTask;
             if (ambientTask != null)
             {
-                if (ambientTask.HasMainThreadSynchronousTaskWaiting)
+                if (JoinableTaskDependencyGraph.HasMainThreadSynchronousTaskWaiting(ambientTask))
                 {
                     return true;
                 }
@@ -337,24 +337,27 @@ namespace Microsoft.VisualStudio.Threading
                 {
                     lock (this.SyncContextLock)
                     {
-                        var allJoinedJobs = new HashSet<JoinableTask>();
                         lock (this.initializingSynchronouslyMainThreadTasks)
                         {
-                            // our read lock doesn't cover this collection
-                            foreach (var initializingTask in this.initializingSynchronouslyMainThreadTasks)
+                            if (this.initializingSynchronouslyMainThreadTasks.Count > 0)
                             {
-                                if (!initializingTask.HasMainThreadSynchronousTaskWaiting)
+                                // our read lock doesn't cover this collection
+                                var allJoinedJobs = new HashSet<JoinableTask>();
+                                foreach (var initializingTask in this.initializingSynchronouslyMainThreadTasks)
                                 {
-                                    // This task blocks the main thread. If it has joined the ambient task
-                                    // directly or indirectly, then our ambient task is considered blocking
-                                    // the main thread.
-                                    initializingTask.AddSelfAndDescendentOrJoinedJobs(allJoinedJobs);
-                                    if (allJoinedJobs.Contains(ambientTask))
+                                    if (!JoinableTaskDependencyGraph.HasMainThreadSynchronousTaskWaiting(initializingTask))
                                     {
-                                        return true;
-                                    }
+                                        // This task blocks the main thread. If it has joined the ambient task
+                                        // directly or indirectly, then our ambient task is considered blocking
+                                        // the main thread.
+                                        JoinableTaskDependencyGraph.AddSelfAndDescendentOrJoinedJobs(initializingTask, allJoinedJobs);
+                                        if (allJoinedJobs.Contains(ambientTask))
+                                        {
+                                            return true;
+                                        }
 
-                                    allJoinedJobs.Clear();
+                                        allJoinedJobs.Clear();
+                                    }
                                 }
                             }
                         }
