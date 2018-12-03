a5297b513ef1f07869f3d23c24f9aba22fc9dd26
Andrew Arnott <andarno@microsoft.com>
2018-12-02 17:54:35 -0800

    Retarget analyzers to Roslyn 2.8 (VS 2017 Update 7)

--- src/Microsoft.VisualStudio.Threading.Analyzers/VSTHRD010MainThreadUsageAnalyzer.cs
+++ src/Microsoft.VisualStudio.Threading.Analyzers/VSTHRD010MainThreadUsageAnalyzer.cs
@@ -9,7 +9,6 @@
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
     using Microsoft.CodeAnalysis.Diagnostics;
-    using Microsoft.CodeAnalysis.Semantics;
     using Microsoft.CodeAnalysis.Text;
 
     /// <summary>
