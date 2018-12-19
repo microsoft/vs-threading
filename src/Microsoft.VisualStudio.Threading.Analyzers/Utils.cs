7be1c9165c275f4f9dce383d674c1580d3c1803b
Andrew Arnott <andarno@microsoft.com>
2018-12-19 10:29:02 -0800

    Split code fixes out into separate project
    Fixes #423

--- src/Microsoft.VisualStudio.Threading.Analyzers/Utils.cs
+++ src/Microsoft.VisualStudio.Threading.Analyzers/Utils.cs
@@ -23,9 +23,6 @@ namespace Microsoft.VisualStudio.Threading.Analyzers
     using CodeAnalysis.Diagnostics;
     using Microsoft;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.FindSymbols;
-    using Microsoft.CodeAnalysis.Rename;
-    using Microsoft.CodeAnalysis.Simplification;
 
     internal static class Utils
     {
