31540f0895edb8669f1c94822e2eb77ebab6b5b7
Sam Harwell <Sam.Harwell@microsoft.com>
2019-11-04 17:14:51 -0800

    Update to Microsoft.CodeAnalysis.Testing 1.0.1-beta1.19554.4

---
+++ src/Microsoft.VisualStudio.Threading.Analyzers.Tests/Helpers/ReferencesHelper.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+
+namespace Microsoft.VisualStudio.Threading.Analyzers.Tests
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis.Testing;
+    using NuGet.Packaging.Core;
+    using NuGet.Versioning;
+
+    internal static class ReferencesHelper
+    {
+        public static ReferenceAssemblies DefaultReferences = ReferenceAssemblies.Default
+            .WithPackages(ImmutableArray.Create(
+                new PackageIdentity("System.Collections.Immutable", NuGetVersion.Parse("1.3.1")),
+                new PackageIdentity("System.Threading.Tasks.Extensions", NuGetVersion.Parse("4.5.3"))));
+    }
+}
