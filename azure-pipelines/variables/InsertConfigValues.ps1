4c927412041b81304607344d46653e947a2a951e
Andrew Arnott <andrewarnott@live.com>
2021-08-30 12:47:02 -0600

    Fix OptProf insertions

--- azure-pipelines/variables/InsertConfigValues.ps1
+++ azure-pipelines/variables/InsertConfigValues.ps1
@@ -7,10 +7,7 @@ if ($dirsToSearch) {
         if ($_.Name -match "^(.*?)\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
             $id = $Matches[1]
             $version = $Matches[2]
-            # Avoid inserting analyzers in an optprof run.
-            if ($env:OPTPROF -ne 'true' -or $_.Name -notlike '*Analyzers*') {
-                $icv += "$id=$version"
-            }
+            $icv += "$id=$version"
         }
     }
 }
--- azure-pipelines/variables/InsertPropsValues.ps1
+++ azure-pipelines/variables/InsertPropsValues.ps1
@@ -3,10 +3,7 @@ $BinPath = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..\bin\Packages\$env:
 $dirsToSearch = "$BinPath\NuGet\*.nupkg" |? { Test-Path $_ }
 $icv=@()
 
-# Avoid updating the .props world for an optprof run.
-# This avoids analyzer updates that are unrelated and can cause build breaks
-# as well as breaks in servicing branches when we insert a PublicRelease=false unstable package version.
-if ($dirsToSearch -and !$env:OPTPROF) {
+if ($dirsToSearch) {
     Get-ChildItem -Path $dirsToSearch |% {
         if ($_.Name -match "^(.*?)\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
             $id = $Matches[1]
