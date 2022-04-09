e90ad8ead100fc0dd2a824cc8485497d3c72b27a
Lifeng Lu <lulifeng@hotmail.com>
2022-04-09 12:06:20 -0700

    Add unit test coverage.
    They were copied from tests for ListOfOftenOne to ensure new data structure to work in the similar way.

---
+++ test/Microsoft.VisualStudio.Threading.Tests/RarelyRemoveItemSetTests.cs
@@ -0,0 +1,141 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq;
+using Microsoft.VisualStudio.Threading;
+using Xunit;
+using Xunit.Abstractions;
+
+public class RarelyRemoveItemSetTests : TestBase
+{
+    private RarelyRemoveItemSet<GenericParameterHelper> list;
+
+    public RarelyRemoveItemSetTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+        this.list = default(RarelyRemoveItemSet<GenericParameterHelper>);
+    }
+
+    [Fact]
+    public void EnumerationOfEmpty()
+    {
+        using (RarelyRemoveItemSet<GenericParameterHelper>.Enumerator enumerator = this.list.EnumerateAndClear().GetEnumerator())
+        {
+            Assert.False(enumerator.MoveNext());
+            enumerator.Reset();
+            Assert.False(enumerator.MoveNext());
+        }
+    }
+
+    [Fact]
+    public void EnumerationOfOne()
+    {
+        this.list.Add(new GenericParameterHelper(1));
+        using (RarelyRemoveItemSet<GenericParameterHelper>.Enumerator enumerator = this.list.EnumerateAndClear().GetEnumerator())
+        {
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal<int>(1, enumerator.Current.Data);
+            Assert.False(enumerator.MoveNext());
+            enumerator.Reset();
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal<int>(1, enumerator.Current.Data);
+            Assert.False(enumerator.MoveNext());
+        }
+    }
+
+    [Fact]
+    public void EnumerationOfTwo()
+    {
+        this.list.Add(new GenericParameterHelper(1));
+        this.list.Add(new GenericParameterHelper(2));
+        using (RarelyRemoveItemSet<GenericParameterHelper>.Enumerator enumerator = this.list.EnumerateAndClear().GetEnumerator())
+        {
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal<int>(1, enumerator.Current.Data);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal<int>(2, enumerator.Current.Data);
+            Assert.False(enumerator.MoveNext());
+            enumerator.Reset();
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal<int>(1, enumerator.Current.Data);
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal<int>(2, enumerator.Current.Data);
+            Assert.False(enumerator.MoveNext());
+        }
+    }
+
+    [Fact]
+    public void RemoveFromEmpty()
+    {
+        this.list.Remove(null!);
+        Assert.Empty(this.list.ToArray());
+        this.list.Remove(new GenericParameterHelper(5));
+        Assert.Empty(this.list.ToArray());
+    }
+
+    [Fact]
+    public void RemoveFromOne()
+    {
+        var value = new GenericParameterHelper(1);
+        this.list.Add(value);
+
+        this.list.Remove(null!);
+        Assert.Single(this.list.ToArray());
+        this.list.Remove(new GenericParameterHelper(5));
+        Assert.Single(this.list.ToArray());
+        this.list.Remove(value);
+        Assert.Empty(this.list.ToArray());
+    }
+
+    [Fact]
+    public void RemoveFromTwoLIFO()
+    {
+        var value1 = new GenericParameterHelper(1);
+        var value2 = new GenericParameterHelper(2);
+        this.list.Add(value1);
+        this.list.Add(value2);
+
+        this.list.Remove(null!);
+        Assert.Equal(2, this.list.ToArray().Length);
+        this.list.Remove(new GenericParameterHelper(5));
+        Assert.Equal(2, this.list.ToArray().Length);
+        this.list.Remove(value2);
+        Assert.Single(this.list.ToArray());
+        Assert.Equal(1, this.list.ToArray()[0].Data);
+        this.list.Remove(value1);
+        Assert.Empty(this.list.ToArray());
+    }
+
+    [Fact]
+    public void RemoveFromTwoFIFO()
+    {
+        var value1 = new GenericParameterHelper(1);
+        var value2 = new GenericParameterHelper(2);
+        this.list.Add(value1);
+        this.list.Add(value2);
+
+        this.list.Remove(null!);
+        Assert.Equal(2, this.list.ToArray().Length);
+        this.list.Remove(new GenericParameterHelper(5));
+        Assert.Equal(2, this.list.ToArray().Length);
+        this.list.Remove(value1);
+        Assert.Single(this.list.ToArray());
+        Assert.Equal(2, this.list.ToArray()[0].Data);
+        this.list.Remove(value2);
+        Assert.Empty(this.list.ToArray());
+    }
+
+    [Fact]
+    public void EnumerateAndClear()
+    {
+        this.list.Add(new GenericParameterHelper(1));
+
+        using (RarelyRemoveItemSet<GenericParameterHelper>.Enumerator enumerator = this.list.EnumerateAndClear().GetEnumerator())
+        {
+            Assert.Empty(this.list.ToArray()); // The collection should have been cleared.
+            Assert.True(enumerator.MoveNext());
+            Assert.Equal(1, enumerator.Current.Data);
+            Assert.False(enumerator.MoveNext());
+        }
+    }
+}
