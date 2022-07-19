af7827a6d5affd4d9a0fc314f599af5febcd0cc5
Jialong Cheng <jiacheng@microsoft.com>
2022-07-19 10:24:19 -0700

    Re-expose a property on whether JoinableTaskContext has any JoinableTask blocking calls on the main thread
    This reverts commit b0cf1501da22e26907a887d2f18adc63f22988b5, reversing
    changes made to 0847b84735ed0e192efa1056cbe7ce05a7aba677.

--- src/Microsoft.VisualStudio.Threading/JoinableTaskFactory.cs
+++ src/Microsoft.VisualStudio.Threading/JoinableTaskFactory.cs
@@ -545,6 +545,12 @@ namespace Microsoft.VisualStudio.Threading
         protected virtual void WaitSynchronouslyCore(Task task)
         {
             Requires.NotNull(task, nameof(task));
+
+            if (this.Context.IsOnMainThread)
+            {
+                this.Context.IncrementMainThreadBlockingCount();
+            }
+
             int hangTimeoutsCount = 0; // useful for debugging dump files to see how many times we looped.
             int hangNotificationCount = 0;
             Guid hangId = Guid.Empty;
@@ -587,6 +593,13 @@ namespace Microsoft.VisualStudio.Threading
                 // Swallow exceptions thrown by Task.Wait().
                 // Our caller just wants to know when the Task completes,
                 // whether successfully or not.
+            }
+            finally
+            {
+                if (this.Context.IsOnMainThread)
+                {
+                    this.Context.DecrementMainThreadBlockingCount();
+                }
             }
         }
 
