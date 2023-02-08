57423065d7124a39c3d96ee31668672d8f22cb85
Andrew Arnott <andrew.arnott@microsoft.com>
2023-02-08 10:15:19 -0700

    Insert the VSInsertionMetadata package into .props file

--- azure-pipelines/variables/InsertPropsValues.ps1
+++ azure-pipelines/variables/InsertPropsValues.ps1
@@ -1,10 +1,8 @@
-$BinPath = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..\bin\Packages\$env:BUILDCONFIGURATION")
+$InsertedPkgs = (& "$PSScriptRoot\..\artifacts\VSInsertion.ps1")
 
-$dirsToSearch = "$BinPath\NuGet\*.nupkg" |? { Test-Path $_ }
 $icv=@()
-
-if ($dirsToSearch) {
-    Get-ChildItem -Path $dirsToSearch |% {
+foreach ($kvp in $InsertedPkgs.GetEnumerator()) {
+    $kvp.Value |% {
         if ($_.Name -match "^(.*?)\.(\d+\.\d+\.\d+(?:\.\d+)?(?:-.*?)?)(?:\.symbols)?\.nupkg$") {
             $id = $Matches[1]
             $version = $Matches[2]
