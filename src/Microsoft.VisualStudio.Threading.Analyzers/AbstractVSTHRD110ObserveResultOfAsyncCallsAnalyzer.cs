f986ae9cf63722824e2685deb905e9503a67d8e7
Andrew Arnott <andrewarnott@live.com>
2025-06-11 06:12:36 -0600

    Reorder members

--- src/Microsoft.VisualStudio.Threading.Analyzers/AbstractVSTHRD110ObserveResultOfAsyncCallsAnalyzer.cs
+++ src/Microsoft.VisualStudio.Threading.Analyzers/AbstractVSTHRD110ObserveResultOfAsyncCallsAnalyzer.cs
@@ -43,57 +43,6 @@ public abstract class AbstractVSTHRD110ObserveResultOfAsyncCallsAnalyzer : Diagn
         });
     }
 
-    private void AnalyzeInvocation(OperationAnalysisContext context, CommonInterest.AwaitableTypeTester awaitableTypes)
-    {
-        var operation = (IInvocationOperation)context.Operation;
-        if (operation.Type is null)
-        {
-            return;
-        }
-
-        if (operation.GetContainingFunction() is { } function && this.LanguageUtils.IsAsyncMethod(function.Syntax))
-        {
-            // CS4014 should already take care of this case.
-            return;
-        }
-
-        // Check if this invocation is within a lambda that's being converted to an Expression<>
-        if (IsWithinExpressionLambda(operation))
-        {
-            // This invocation is within a lambda converted to an expression tree, so it's not actually being invoked.
-            return;
-        }
-
-        // Only consider invocations that are direct statements (or are statements through limited steps).
-        // Otherwise, we assume their result is awaited, assigned, or otherwise consumed.
-        IOperation? parentOperation = operation.Parent;
-        while (parentOperation is not null)
-        {
-            if (parentOperation is IExpressionStatementOperation)
-            {
-                // This expression is directly used in a statement.
-                break;
-            }
-
-            // This check is where we allow for specific operation types that may appear between the invocation
-            // and the statement that don't disqualify the invocation search for an invalid pattern.
-            if (parentOperation is IConditionalAccessOperation)
-            {
-                parentOperation = parentOperation.Parent;
-            }
-            else
-            {
-                // This expression is not directly used in a statement.
-                return;
-            }
-        }
-
-        if (awaitableTypes.IsAwaitableType(operation.Type))
-        {
-            context.ReportDiagnostic(Diagnostic.Create(Descriptor, operation.Syntax.GetLocation()));
-        }
-    }
-
     /// <summary>
     /// Determines whether an invocation is within a lambda expression that is being converted to an Expression tree.
     /// </summary>
@@ -184,5 +133,56 @@ public abstract class AbstractVSTHRD110ObserveResultOfAsyncCallsAnalyzer : Diagn
         }
 
         return false;
+    }
+
+    private void AnalyzeInvocation(OperationAnalysisContext context, CommonInterest.AwaitableTypeTester awaitableTypes)
+    {
+        var operation = (IInvocationOperation)context.Operation;
+        if (operation.Type is null)
+        {
+            return;
+        }
+
+        if (operation.GetContainingFunction() is { } function && this.LanguageUtils.IsAsyncMethod(function.Syntax))
+        {
+            // CS4014 should already take care of this case.
+            return;
+        }
+
+        // Check if this invocation is within a lambda that's being converted to an Expression<>
+        if (IsWithinExpressionLambda(operation))
+        {
+            // This invocation is within a lambda converted to an expression tree, so it's not actually being invoked.
+            return;
+        }
+
+        // Only consider invocations that are direct statements (or are statements through limited steps).
+        // Otherwise, we assume their result is awaited, assigned, or otherwise consumed.
+        IOperation? parentOperation = operation.Parent;
+        while (parentOperation is not null)
+        {
+            if (parentOperation is IExpressionStatementOperation)
+            {
+                // This expression is directly used in a statement.
+                break;
+            }
+
+            // This check is where we allow for specific operation types that may appear between the invocation
+            // and the statement that don't disqualify the invocation search for an invalid pattern.
+            if (parentOperation is IConditionalAccessOperation)
+            {
+                parentOperation = parentOperation.Parent;
+            }
+            else
+            {
+                // This expression is not directly used in a statement.
+                return;
+            }
+        }
+
+        if (awaitableTypes.IsAwaitableType(operation.Type))
+        {
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, operation.Syntax.GetLocation()));
+        }
     }
 }
